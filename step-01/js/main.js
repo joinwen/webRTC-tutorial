78d044e70a84da15c9590fa907748ea0526f8c98
cywen <18205055638@163.com>
2021-10-27 11:36:30 +0800

    fix: call play

--- step-01/js/main.js
+++ step-01/js/main.js
@@ -1,18 +1,20 @@
 const localVideoEle = document.querySelector("#video");
-
+let localStream = null;
 /**
- * 获取本地媒体资源成功
+ * 获取本地媒体资源失败
  */
 const handleLocalStreamError = (error) => {
     console.error("get local stream error",error);
 }
 
 /**
- * 获取本地媒体资源失败
+ * 获取本地媒体资源成功
  */
 const handleLocalStreamSuccess = (stream) => {
     console.info("get local stream success");
+    localStream = stream;
     localVideoEle.srcObject = stream;
+    localVideoEle.play();
 }
 
 navigator.mediaDevices.getUserMedia({audio: false, video: true})
